3fe161bf170786045866c933678b23c72e6a52b5
marcobusacca <marcobusacca01@gmail.com>
2024-02-16 18:36:32 +0100

    [FEAT] Application.java seeder for Task objects

--- src/main/java/it/mytools/spring/MyToolsServerApplication.java
+++ src/main/java/it/mytools/spring/MyToolsServerApplication.java
@@ -1,13 +1,18 @@
 package it.mytools.spring;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import it.mytools.spring.db.pojo.Task;
+import it.mytools.spring.db.serv.TaskService;
 
 @SpringBootApplication
 public class MyToolsServerApplication implements CommandLineRunner {
+
+	@Autowired
+	private TaskService taskService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyToolsServerApplication.class, args);
@@ -16,8 +21,11 @@ public class MyToolsServerApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		Task t1 = new Task("task-1");
-
+		Task t2 = new Task("task-2");
+		taskService.save(t1);
+		taskService.save(t2);
 		System.out.println(t1);
+		System.out.println(t2);
 	}
 
 }
