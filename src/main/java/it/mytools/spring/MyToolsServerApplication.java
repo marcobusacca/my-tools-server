e4677b9ec64b737ca112c2002a7fd0d947d485e0
marcobusacca <marcobusacca01@gmail.com>
2024-02-16 18:31:48 +0100

    [FEAT] db.repo Repository "TaskRepository"

--- src/main/java/it/mytools/spring/MyToolsServerApplication.java
+++ src/main/java/it/mytools/spring/MyToolsServerApplication.java
@@ -1,13 +1,23 @@
 package it.mytools.spring;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import it.mytools.spring.db.pojo.Task;
+
 @SpringBootApplication
-public class MyToolsServerApplication {
+public class MyToolsServerApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyToolsServerApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Task t1 = new Task("task-1");
+
+		System.out.println(t1);
 	}
 
 }
