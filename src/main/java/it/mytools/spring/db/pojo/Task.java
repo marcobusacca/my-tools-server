504609b9e421c5867d6b4fcb4ad749b457d7cfb0
marcobusacca <marcobusacca01@gmail.com>
2024-02-16 18:26:25 +0100

    [FEAT] db.pojo Entity "Task"

---
+++ src/main/java/it/mytools/spring/db/pojo/Task.java
@@ -0,0 +1,84 @@
+package it.mytools.spring.db.pojo;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String title;
+
+    private LocalDate date;
+
+    private LocalTime time;
+
+    private boolean done;
+
+    public Task() {
+    }
+
+    public Task(String title) {
+        setTitle(title);
+        setDone(false);
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public LocalDate getDate() {
+        return this.date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public LocalTime getTime() {
+        return this.time;
+    }
+
+    public void setTime(LocalTime time) {
+        this.time = time;
+    }
+
+    public boolean isDone() {
+        return this.done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                " id='" + getId() + "'" +
+                ", title='" + getTitle() + "'" +
+                ", date='" + getDate() + "'" +
+                ", time='" + getTime() + "'" +
+                ", done='" + isDone() + "'" +
+                "}";
+    }
+}
