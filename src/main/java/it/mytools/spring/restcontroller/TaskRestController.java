f5d916eecf8121a8634895e6d9f998aaa174f565
marcobusacca <marcobusacca01@gmail.com>
2024-02-17 12:17:30 +0100

    [FIX]

--- src/main/java/it/mytools/spring/restcontroller/TaskRestController.java
+++ src/main/java/it/mytools/spring/restcontroller/TaskRestController.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,7 +29,7 @@ public class TaskRestController {
         return new ResponseEntity<>(tasks, HttpStatus.OK);
     }
 
-    @PostMapping("/done/{id}")
+    @PutMapping("/done/{id}")
     public ResponseEntity<String> setTaskDone(@PathVariable int id) {
 
         Task task = taskService.findById(id);
