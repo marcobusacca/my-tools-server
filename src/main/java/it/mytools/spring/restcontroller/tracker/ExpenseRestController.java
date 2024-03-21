77edfd33b2f3cb350d789ab643d55618b362cd70
marcobusacca <marcobusacca01@gmail.com>
2024-03-21 13:36:22 +0100

    [FEAT] ExpenseRestController getExpenses method

--- src/main/java/it/mytools/spring/restcontroller/tracker/ExpenseRestController.java
+++ src/main/java/it/mytools/spring/restcontroller/tracker/ExpenseRestController.java
@@ -1,9 +1,15 @@
 package it.mytools.spring.restcontroller.tracker;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import it.mytools.spring.db.pojo.tracker.Expense;
 import it.mytools.spring.db.serv.tracker.ExpenseService;
 
 @RestController
@@ -12,4 +18,12 @@ public class ExpenseRestController {
 
     @Autowired
     private ExpenseService expenseService;
+
+    @GetMapping
+    public ResponseEntity<List<Expense>> getExpenses() {
+
+        List<Expense> expenses = expenseService.findAll();
+
+        return new ResponseEntity<>(expenses, HttpStatus.OK);
+    }
 }
