7c5b8f3059c7c9b54deaab04b3d6c571e6cdfab6
marcobusacca <marcobusacca01@gmail.com>
2024-03-22 12:49:47 +0100

    [FEAT] WalletRestController POST method "storeWallet"

--- src/main/java/it/mytools/spring/restcontroller/tracker/WalletRestController.java
+++ src/main/java/it/mytools/spring/restcontroller/tracker/WalletRestController.java
@@ -7,7 +7,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,6 +29,19 @@ public class WalletRestController {
         List<Wallet> wallets = walletService.findAll();
 
         return new ResponseEntity<>(wallets, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<String> storeWallet(@RequestBody Wallet walletForm) {
+
+        String walletName = walletForm.getName();
+        double walletBalance = walletForm.getBalance();
+
+        Wallet wallet = new Wallet(walletName, walletBalance);
+
+        walletService.save(wallet);
+
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @PutMapping("/active/{id}")
